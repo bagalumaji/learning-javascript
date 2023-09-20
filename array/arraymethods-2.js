50948af8851af85c8c67af315854f0c281fb1ca7
bagalumaji <bagalumaji@gmail.com>
2023-09-20 20:47:58 +0530

    indexof, find, valueof

---
+++ array/arraymethods-2.js
@@ -0,0 +1,37 @@
+
+//entries() - Returns an iterator over the key-value pairs of an array.
+// lastIndex() - 
+// indexOf() -
+// includes
+// find
+
+
+const arr = [1, 2, 3, 4, 5, 6, 7]
+
+let val = arr.lastIndexOf('d');
+
+console.log(val);
+
+console.log(arr.indexOf('d'));
+
+console.log(arr);
+
+const [[...b]] = arr.entries();
+
+//console.log(b)
+
+console.log(arr.includes(3))
+
+console.log(arr.find(value => value === 2));
+
+console.log( arr.values().next());
+
+
+arr.filter(value => value % 2 ===0).forEach(value => console.log(value));
+
+arr.map(value=>value*2).forEach(value => console.log(value));
+
+arr.reduce()
+
+
+
