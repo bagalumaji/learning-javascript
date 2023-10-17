92e45a86a17a47ade0b27d9ce48d12647b1e0239
Umaji.Bagal <bagalumaji@gmail.com>
2023-10-17 12:24:12 +0530

    added constructor

--- objects/createobject.js
+++ objects/createobject.js
@@ -3,5 +3,15 @@
 const obj = {
   name: "sayaji",
   address: "gadegaon",
-  isEmpolyee: falsel,
+  isEmpolyee: false,
 };
+
+//
+function Person(name, email) {
+  this.name = name;
+  this.email = email;
+}
+
+const person = new Person("sayaji", "sayajibagal@gmail.com");
+console.log(person.name);
+console.log(person.email);
