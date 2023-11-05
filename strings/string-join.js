5de0c0c0fb51d5e10a49b0b952ea259083d070d2
bagalumaji <bagalumaji@gmail.com>
2023-11-05 10:18:18 +0530

    string join operation

---
+++ strings/string-join.js
@@ -0,0 +1,13 @@
+const s1 = "first";
+const s2 = "second";
+const s3 = "third";
+
+const result1 = s1 + s2 + s3;
+
+const result2 = `${s1}${s2}${s3}`;
+
+const result3 = s1.concat(s2, s3);
+
+console.log("Result 1 : ", result1);
+console.log("Result 2 : ", result2);
+console.log("Result 3 : ", result3);
