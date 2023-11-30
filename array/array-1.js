b1c0a18b577102e14e2f1cf683d4a306fb420ac0
bagalumaji <bagalumaji@gmail.com>
2023-11-30 10:29:00 -0800

    array methods : map, filter

--- array/array-1.js
+++ array/array-1.js
@@ -18,7 +18,11 @@ const arr3 = arr1.concat(arr2);
 //console.log(arr3);
 
 const newArr = [...arr1,...arr2];
-console.log(newArr);
+//console.log(newArr);
 
-console.log(newArr.slice(2,5));
-console.log(arr1.splice(2, 2));
+//console.log(newArr.slice(2,5));
+//console.log(arr1.splice(2, 2));
+
+//arr2.map(val=>val*2).forEach(e=>console.log(e))
+
+arr1.filter(v=> typeof v === "number").map(v=>v*v).forEach(v=>console.log(v))
