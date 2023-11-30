46e1eb80d4c2a16bf468ab29bf46197700bd5e50
bagalumaji <bagalumaji@gmail.com>
2023-11-30 10:06:53 -0800

    array methods

---
+++ array/array-1.js
@@ -0,0 +1,24 @@
+const arr1 = [1,2,3,'hi'];
+arr1.push(5);
+//console.log(arr1);
+arr1.unshift(7);
+//console.log(arr1);
+arr1.pop();
+arr1.shift();
+//console.log(arr1);
+arr1.forEach(val=>console.log(val));
+
+for(let key in arr1){
+console.log(key,arr1[key]);
+}
+const arr2 = [8,9];
+
+const arr3 = arr1.concat(arr2);
+
+//console.log(arr3);
+
+const newArr = [...arr1,...arr2];
+console.log(newArr);
+
+console.log(newArr.slice(2,5));
+console.log(arr1.splice(2, 2));
