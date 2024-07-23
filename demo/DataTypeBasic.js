ee97d6aab2adff8cefd1ec83be1ea6970411813a
Umaji Bagal <63182777+bagalumaji@users.noreply.github.com>
2024-07-23 04:36:35 +0000

    data type basic - 1

---
+++ demo/DataTypeBasic.js
@@ -0,0 +1,34 @@
+
+// data type
+
+//numeric
+    //int
+    //float
+    // NaN
+    // Infinity
+    // -Infinity
+
+    let total=20;
+    let percentage=33.33;
+    let score=Infinity;
+    const pi=3.14;
+    total=100;
+    
+    console.log(total);
+    
+    console.log("Total : "+total);
+    console.log('Percentage : '+percentage);
+    console.log("Score : "+score);
+    
+    //type
+    //typeof 
+    console.log("type of total - ",typeof(total));
+    console.log("type of percrntage : ",typeof percentage);
+    console.log("type of score : ",typeof score);
+    let a=NaN;
+    console.log("Values of A :",a);
+    console.log("type of NaN : ",typeof a);
+    
+    let value='1.23';
+    console.log(value-10);
+    
