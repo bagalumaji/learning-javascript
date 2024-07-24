5819ca6b3335b36ed9feadb96e057923beb30eb5
Umaji Bagal <63182777+bagalumaji@users.noreply.github.com>
2024-07-24 03:00:11 +0000

    basic data type

---
+++ datatype/BasicDataType.js
@@ -0,0 +1,36 @@
+
+console.log("hello world");
+
+// data type
+
+// variable 
+
+//let , const, var
+
+let a=10;
+a=22;
+
+const name1="Abc";
+
+
+var total=80;
+
+// numeric
+// integer
+//float
+// Infinity
+//-Infinity
+//NaN
+
+let num=1;
+num=1.2;
+num=Infinity;
+num = -Infinity;
+num=NaN;
+
+let type = typeof num;
+console.log(type)
+
+console.log("Num : ",num);
+console.log("Num : "+num);
+
