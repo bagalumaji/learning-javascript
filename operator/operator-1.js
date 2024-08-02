8254dd516c47260f6dbfd09133ee79d7568cc2b5
Umaji Bagal <63182777+bagalumaji@users.noreply.github.com>
2024-08-02 02:42:24 +0000

    operator

---
+++ operator/operator-1.js
@@ -0,0 +1,30 @@
+//+
+
+let str1="hello";
+let str2="world";
+
+console.log(str1+" "+str2)
+
+let a=0;
+a= a + 10;
+a+=10;
+
+str1+=" wolrd"
+
+//
+// ? :
+// exp1 ? exp2 : exp3 ;
+
+//2 largest
+ a=100;
+ let b=20;
+
+ const largest = a > b ? a : b;
+console.log(largest)
+
+ a = 5 ** 2;
+
+ console.log("** : ",a);
+
+a = 100;
+
