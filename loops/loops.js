02611bdf654ecc63ed249465f8281ff5064bd7ff
Umaji Bagal <63182777+bagalumaji@users.noreply.github.com>
2024-08-02 02:42:17 +0000

    loops

---
+++ loops/loops.js
@@ -0,0 +1,33 @@
+
+
+//console.log("hello...") // 10
+
+//for
+//while
+//do-while
+
+//for
+
+for(let i=10;1;i--){//11
+    // console.log(i) // 10 9 8 7 ...1
+    if(i==5)
+        break;
+    }
+    ///
+    
+    let i=1;
+    while(1){
+        console.log(i);
+        if(i==4)
+            break;
+        i++;
+    }
+    
+    i=12;
+    do{
+        console.log(i);
+        i++;
+    }while(i<=10);
+    /// 
+    
+    
