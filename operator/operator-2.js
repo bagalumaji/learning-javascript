2cbca05133c436fad3a17a1df47250a7caa6bd1d
Umaji Bagal <63182777+bagalumaji@users.noreply.github.com>
2024-08-02 02:42:32 +0000

    operator

---
+++ operator/operator-2.js
@@ -0,0 +1,49 @@
+
+// ++,--
+
+let a=5;
+
+console.log("A : ",a);
+
+let c = ++a;
+console.log("c : ",c," A :",a);
+
+
+c = a++;
+console.log("c : ",c," A :",a);
+
+c = --a;
+console.log("c : ",c," A :",a);
+
+c = a--;
+console.log("c : ",c," A :",a);
+
+/// == -> type
+/// === -> type and value
+ let s = "1"-2;
+ 
+ console.log(1===1);
+console.log("logical");
+ console.log(!true);
+
+a =1;
+let b=20;
+// console.log(a>10 && a<b && a==3&&a!=10);//false
+// console.log(a==2 || a<2);
+console.log(a==1 & a!=1);//false
+console.log(a==2 | a!=2); //true
+
+//5
+//1
+
+//0101 ==5
+//0001 == 1
+//-------
+// 0001==>1
+
+//1001<<1
+//1101>>1
+//1010>>>1
+
+
+
