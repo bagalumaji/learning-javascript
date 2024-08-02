4a6a718885c18499f20c228ad1e7e7299dabb5fb
Umaji Bagal <63182777+bagalumaji@users.noreply.github.com>
2024-08-02 02:38:06 +0000

    functions

---
+++ functions/function-1.js
@@ -0,0 +1,29 @@
+//no para no return
+//no para with return
+//para with no return
+//para with return
+
+//no para no return
+
+function print(){
+    console.log("hello");
+}
+
+//no para with return
+
+function getHelloInUpperCase(){
+    return "HELLO";
+}
+
+
+//para with no return
+
+function add(a,b){
+    console.log(a+b);
+}
+
+//para with  return
+function add1(a,b){
+    return a+b;
+}
+
