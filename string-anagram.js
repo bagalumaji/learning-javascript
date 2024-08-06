6ad684c7d2b1080131a911119cece0158f7706b1
Umaji Bagal <63182777+bagalumaji@users.noreply.github.com>
2024-08-06 04:46:12 +0000

    string anagram

---
+++ string-anagram.js
@@ -0,0 +1,12 @@
+
+let str1 = "hello";
+let str2 = "hi"
+str1 = str1.split('').sort().join('');
+str2 = str2.split('').sort().join('');
+console.log(str1);
+console.log(str2);
+if (str1 == str2) {
+    console.log("strings are anagram");
+} else {
+    console.log("strings are NOT anagram");
+}
