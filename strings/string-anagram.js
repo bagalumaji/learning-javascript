fa252103da63aac9e5202054dbdae8faf4188dd7
umajibagal <bagalumaji@gmail.com>
2024-08-07 22:19:53 -0700

    updated operator to ====

--- strings/string-anagram.js
+++ strings/string-anagram.js
@@ -5,7 +5,7 @@ str1 = str1.split('').sort().join('');
 str2 = str2.split('').sort().join('');
 console.log(str1);
 console.log(str2);
-if (str1 == str2) {
+if (str1 === str2) {
     console.log("strings are anagram");
 } else {
     console.log("strings are NOT anagram");
