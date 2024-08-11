46e973599a646886e999ce5ab80cb16fc8446098
umajibagal <bagalumaji@gmail.com>
2024-08-11 11:53:46 +0530

    interview

---
+++ interview/interview-4.js
@@ -0,0 +1,10 @@
+
+// hello();
+// function  hello(){
+//     console.log("hello")
+// }
+// it will work bcz hoisting
+
+// new Demo();// it will throw error-ReferenceError: Cannot access 'Demo' before initialization
+// because --> let const and class are not hoisted
+class Demo{}
