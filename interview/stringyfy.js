e84726eb603d7b78dc401c131eba71ea49bfbbbd
umajibagal <bagalumaji@gmail.com>
2024-08-11 11:53:50 +0530

    interview

---
+++ interview/stringyfy.js
@@ -0,0 +1,8 @@
+console.log(JSON.stringify("JS") === "JS");
+console.log(JSON.stringify("JS"));
+console.log("JS");
+console.log(JSON.stringify(null));
+console.log(JSON.stringify(1));
+console.log(JSON.stringify([]));
+console.log(JSON.stringify({}));
+console.log(JSON.stringify(undefined));
