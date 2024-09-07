2f06b5a0121a3e31d26ce7a8586f9f8bb24ba2d0
Umaji Bagal <bagalumaji@gmail.com>
2024-09-07 10:59:29 +0530

    multi level inheritance

--- classes/multilevel-inheritance.js
+++ classes/multilevel-inheritance.js
@@ -1,21 +1,32 @@
 class A{
-   show() {
+   constructor() {
+       console.log("constructor - class A");
+   }
+    show() {
        console.log("show from A")
    }
 }
 class B extends A{
+    constructor() {
+        console.log("constructor - class B");
+        super()
+    }
+
     display(){
         console.log("hello from B")
     }
 }
 class C extends B{
+    constructor() {
+        console.log("constructor - class C");
+        super();
+    }
     print(){
         console.log("hello from C")
     }
 }
 
 const c = new C();
-console.log(c);
 c.print();
 c.display()
 c.show();
