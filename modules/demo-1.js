95eedde70584b80b89fe4cea289ae92f559e2342
Umaji Bagal <bagalumaji@gmail.com>
2024-09-09 07:46:17 +0530

    module

---
+++ modules/demo-1.js
@@ -0,0 +1,10 @@
+import Abc from '../classes/class-demo-1.js'
+import {obj,print,firstName} from '../classes/class-demo-1.js'
+
+
+const x=new Abc("Ram1");
+console.log(x.name);
+x.show();
+console.log(obj);
+print();
+console.log(firstName);
