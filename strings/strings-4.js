8c718219bc3b0c789023cffb5ab06d9c5b583c6b
umaji bagal <bagalumaji@gmail.com>
2024-11-17 21:43:36 +0530

    string methods

--- strings/strings-4.js
+++ strings/strings-4.js
@@ -15,3 +15,19 @@ console.log(str.toLowerCase());
 // includes to check sub string present or not
 
 console.log(str.includes("world"));
+
+console.log(str.split(" "));
+
+console.log(str.indexOf("world"));
+
+console.log(str.lastIndexOf("hello"));
+console.log(str.at(4));
+console.log(str.charAt(9));
+console.log(str.charCodeAt(2));
+console.log(str.concat(" to programming"));
+console.log(str.replace("hello", "Hello"));
+console.log(str.replaceAll("o", "O"));
+console.log(str.substring(1));
+console.log(str.substring(0, 5));
+console.log(str.endsWith("world"));
+console.log(str.startsWith("hello"));
