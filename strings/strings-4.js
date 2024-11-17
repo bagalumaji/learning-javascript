85b4e38e302aa2175865ace2dee73408b3e7f2fc
umaji bagal <bagalumaji@gmail.com>
2024-11-17 21:22:51 +0530

    string methods

---
+++ strings/strings-4.js
@@ -0,0 +1,17 @@
+const str="hello world";
+console.log(str);
+
+//length of string
+console.log(str.length);
+
+// to upper case
+
+console.log(str.toUpperCase());
+
+
+//to lower case
+console.log(str.toLowerCase());
+
+// includes to check sub string present or not
+
+console.log(str.includes("world"));
